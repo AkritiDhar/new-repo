f58b36493d5fb907b3a0b64ab86186cb196c3927
Akriti <akritidhar1@gmail.com>
2020-08-16 17:27:46 +0530

    Initial Commit

---
+++ simple-maven-artifact/src/main/java/simple_maven/simple_maven_artifact/App.java
@@ -0,0 +1,13 @@
+package simple_maven.simple_maven_artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ simple-maven-artifact/src/test/java/simple_maven/simple_maven_artifact/AppTest.java
@@ -0,0 +1,20 @@
+package simple_maven.simple_maven_artifact;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
